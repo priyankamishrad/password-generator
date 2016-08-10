377c10515f77d3a01f587c57e914d031ad83bce2
Priyanka Mishra <pmishradube@gmail.com>
2016-08-10 02:36:13 -0400

    Updated readable_password.php

--- readable_password.php
+++ readable_password.php
@@ -1,45 +1,111 @@
-
-
 <?php
-//this will read the dicitonary friendly_words.txt
-function read_dictionary($filename = "") {
-  $dictionary_file = "dictionaries/$filename";
-  return file($dictionary_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+function detect_any_uppercase($string) {
+  //true if lowercasing the string
+  return strtolower($string) != $string;
 }
 
-//function to pick a random word from the $word array
-function pick_random($array) {
-  $i = mt_rand(0,count($array) -1);
-  return $array[$i];
+function detect_any_lowercase($string) {
+  //true if uppercasing the string
+  return strtoupper($string) != $string;
 }
 
-//function to pick a random symbol.
-function pick_random_symbol() {
-    $symbols = '!&$?-';
-    $i = mt_rand(0, strlen($symbols) -1);
-    return $symbols[$i];
+function count_numbers($string) {
+  return preg_match_all('/[0-9]/',$string);
 }
 
-//function to pick a random numbers.
-function pick_random_number($digit) {
-  $min = pow(10, ($digit -1));
-  $max = pow(10,($digit)) -1;
-  return strval(mt_rand($min,$max));
+function count_symbols($string) {
+  $regex = '/[' . preg_quote('!@#$%^&*-_+=?') .']/';
+  return preg_match_all($regex,$string);
 }
 
-$basic_words = read_dictionary('friendly_words.txt');
-$brand_words = read_dictionary('brand_words.txt');
-$small_words = read_dictionary('small_words.txt');
+function password_strength($password) {
+  $strength = 0;
+  $possible_points = 12;
+  $length = strlen($password);
 
-$words = array_merge($brand_words, $basic_words);
+  if(detect_any_uppercase($password)) {
+    $strength += 1;
+  }
 
-//$length = 12;
-$password = "";
-$password .= pick_random($words);
-$password .= pick_random_symbol();
-$password .= pick_random_number(2);
-$password .= pick_random($small_words);
+  if(detect_any_lowercase($password)) {
+    $strength += 1;
+  }
 
-echo "Friendly password: " . $password . "<br>";
-echo "Length: " . strlen($password) . "<br>";
+  $strength += min(count_numbers($password),2);
+  $strength += min(count_symbols($password),2);
+
+  if($length >= 8) {
+    $strength += 2;
+    $strength += min(($length - 8) * 0.5, 4);
+
+  $strength_percent = $strength / (float) $possible_points;
+  $rating = floor($strength_percent * 10);
+  return $rating;
+}
+}
+
+$password = $_POST['rate'];
+$rating = password_strength($password);
+
 ?>
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+    <meta charset = "utf-8">
+		<title>Password Strength Meter</title>
+
+    <style>
+      #meter div {
+        height: 10px;
+        width: 10px;
+        border-radius: 5px;
+        margin: 0 1px 0 0;
+        float: left;
+        background-color: #DDDDDD;
+      }
+
+      #meter div.rating-1, #meter div.rating-2  {
+        background-color: green;
+      }
+      #meter div.rating-3, #meter div.rating-4  {
+        background-color: blue;
+      }
+      #meter div.rating-5, #meter div.rating-6  {
+        background-color: yellow;
+      }
+      #meter div.rating-7, #meter div.rating-8  {
+        background-color: orange;
+      }
+      #meter div.rating-9, #meter div.rating-10  {
+        background-color: red;
+      }
+
+    </style>
+	</head>
+
+	<body>
+    <p> Your password rating is: <?php echo $rating; ?></p>
+    <div id = "meter">
+      <?php
+        for($i=0; $i<10; $i++) {
+          echo "<div";
+          if($rating > $i){
+            echo " class= \"rating-{$rating}\"";
+          }
+          echo "></div>";
+        }
+      ?>
+    </div><!--meter-->
+
+    <br style = "clear: both">
+    <p> Rate the strength of your password: </p>
+    <form action = "" method = "post">
+      Password : <input type = "text" name = "rate" value = ""><br>
+      <input type = "submit" value = "Submit">
+    </form>
+
+	</body>
+</html>
